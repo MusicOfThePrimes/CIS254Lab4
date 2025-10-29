595cea169543849145e4a2b21a9e35b291390c63
Huilin Reid <huilin.reid@gmail.com>
2025-10-28 18:48:02 -0700

    clean commented lines

--- Application.java
+++ Application.java
@@ -25,25 +25,22 @@ public class Application {
         account.withdraw(200);
         System.out.println("New Balance: " + account.getBalance());  // print balance
 
-        // System.out.println("*************************************");
-
         System.out.println("\nAccount statements:");
         System.out.println("---------------------------------------------------------------------------");
         System.out.println(account.getStatement());
     
-        System.out.println("Account balance: " + account.getBalance());  // print account balance
+        System.out.println("Account balance: " + account.getBalance());  // print account balance, double
 
-        // System.out.println("\nMore Accounts, Balance, Statements:");
         System.out.println("\n***************************************************************************");
-        BankAccount account2 = new BankAccount(50);  // bank account 2
+        BankAccount account2 = new BankAccount(50);  // create bank account 2
         System.out.println("Account " + account2.getAccountNumber() + "\nBalance: " + account2.getBalance());  // print account 2
         System.out.println("Statements:\n" + account2.getStatement());  // print statements
 
-        BankAccount account3 = new BankAccount(150);  // bank account 3
+        BankAccount account3 = new BankAccount(150);  // create bank account 3
         System.out.println("Account " + account3.getAccountNumber() + "\nBalance: " + account3.getBalance());  // print account 3
         System.out.println("Statements:\n" + account3.getStatement());  // print statements
 
-        BankAccount account4 = new BankAccount();  // bank account 4 
+        BankAccount account4 = new BankAccount();  // create bank account 4 
         System.out.println("Account " + account4.getAccountNumber() + "\nBalance: " + account4.getFormattedBalance());  // print account 4
         System.out.println("Statements:\n" + account4.getStatement());
         
@@ -51,7 +48,7 @@ public class Application {
         account4.deposit(-100);
         account4.deposit(888.88);
 
-        System.out.println("\nBalance: " + account4.getFormattedBalance());  // print account balance, formatted 
+        System.out.println("\nBalance: " + account4.getFormattedBalance());  // print account balance, formatted, string 
         System.out.println("Statements:\n" + account4.getStatement());
     }
 }
